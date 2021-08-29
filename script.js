7e165db98a77945bf1f329bf87da7a6267c70cc2
AmulyaGulati <amulyagulati@gmail.com>
2021-08-29 23:05:20 +0530

    Bankist Repository

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ const account2 = {
 };
 
 const account3 = {
-  owner: 'Akash Kumar',
+  owner: 'Akash',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 0407,
