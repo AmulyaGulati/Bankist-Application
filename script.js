38a0f87e52315311479e280becc240560be86b0e
AmulyaGulati <amulyagulati@gmail.com>
2021-09-13 22:06:02 +0530

    Remove Comments

--- script.js
+++ script.js
@@ -5,48 +5,6 @@
 /////////////////////////////////////////////////
 // Data
 
-// DIFFERENT DATA! Contains movement dates, currency and locale
-
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111,
-
-//   movementsDates: [
-//     '2019-11-18T21:31:17.178Z',
-//     '2019-12-23T07:42:02.383Z',
-//     '2020-01-28T09:15:04.904Z',
-//     '2020-04-01T10:17:24.185Z',
-//     '2020-05-08T14:11:59.604Z',
-//     '2020-07-26T17:01:17.194Z',
-//     '2020-07-28T23:36:17.929Z',
-//     '2020-08-01T10:51:36.790Z',
-//   ],
-//   currency: 'EUR',
-//   locale: 'pt-PT', // de-DE
-// };
-
-// const account2 = {
-//   owner: 'Jessica Davis',
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 2222,
-
-//   movementsDates: [
-//     '2019-11-01T13:15:33.035Z',
-//     '2019-11-30T09:48:16.867Z',
-//     '2019-12-25T06:04:23.907Z',
-//     '2020-01-25T14:18:46.235Z',
-//     '2020-02-05T16:33:06.386Z',
-//     '2020-04-10T14:43:26.374Z',
-//     '2020-06-25T18:49:59.371Z',
-//     '2020-07-26T12:01:20.894Z',
-//   ],
-//   currency: 'USD',
-//   locale: 'en-US',
-// };
-
 const account1 = {
   owner: 'Amulya Gulati',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -292,11 +250,6 @@ const startLogOutTimer = function () {
 // Event handlers
 let currentAccount, timer;
 
-// FAKE ALWAYS LOGGED IN
-// currentAccount = account1;
-// updateUI(currentAccount);
-// containerApp.style.opacity = 100;
-
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
@@ -330,13 +283,6 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.locale,
       options
     ).format(now);
-
-    // const day = `${now.getDate()}`.padStart(2, 0);
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, 0);
-    // const min = `${now.getMinutes()}`.padStart(2, 0);
-    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
